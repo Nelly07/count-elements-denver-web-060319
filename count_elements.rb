5b927940fb9b8fe69782dfc3461deb0f7469c450
Nelly <nkhmeleva@yahoo.com>
2019-06-10 11:36:37 -0600

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,22 @@
+require "pry"
 def count_elements(array)
   # code goes here
+  new_hash={}
+  
+  array.each do |e|
+    if !new_hash.has_key?(e)
+       new_hash[e] = 1
+    else 
+      new_hash[e]+= 1
+       
+    end 
+    
+  
+  end 
+ new_hash
 end
+
  
+# animals = ['tortoise', 'aye-aye', 'honey badger', 'aye-aye', 'tortoise', 'tortoise']
+# count_elements(animals)
+# => {'tortoise' => 3, 'aye-aye' => 2, 'honey badger' => 1}
